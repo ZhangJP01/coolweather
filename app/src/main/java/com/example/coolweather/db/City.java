6c9bbb35c12716c1c243d7ff63806c9ed8ededb3
zjp <ZhangJP5519@163.com>
2022-03-28 16:03:55 +0800

    添加继承Litepal数据库

--- app/src/main/java/com/example/coolweather/db/City.java
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -1,9 +1,11 @@
 package com.example.coolweather.db;
 
+import org.litepal.crud.DataSupport;
+
 /**
  * 市  实体类
  */
-public class City {
+public class City extends DataSupport {
 
     private int id;
     private String cityName;
